90c01af2ca28a0f93b4dceeeab98358d82c6ae11
unknown <martimramos2013@gmail.com>
2025-10-09 10:49:33 +0100

    primeiro ficheiro

---
+++ ex001.py
@@ -0,0 +1,4 @@
+nota1 = int(input ("Digite a primeira nota:"))
+nota2 = int(input ("Digite a segunda nota :"))
+soma = nota1 + nota2
+print("A primeira é (), segunda é () soma das notas é : () " .format (nota1, nota2,soma))
